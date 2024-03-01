043de0cf55a4dadb33272e04f8e42a00dc19ea60
Coastal Hydrology Lab <161363853+CHL-UA@users.noreply.github.com>
2024-02-29 22:03:06 -0600

    Update sample.py

--- scripts/sample.py
+++ scripts/sample.py
@@ -1,1 +1,12 @@
+# [Project Name]
 
+## Author
+Name: [Your Full Name]  
+Email: [Your Email Address]  
+Affiliation: PhD Student, University of Alabama  
+
+## Description
+[Project description here]
+
+## License
+This software is licensed under the Apache License 2.0. See the LICENSE file for more details.
